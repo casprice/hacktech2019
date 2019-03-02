31d54cf8ef0df1347b1a53ea7d2e6b89940e8b0c
casprice <c2price@ucsd.edu>
2019-03-02 03:55:46 -0800

    added initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+// https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
